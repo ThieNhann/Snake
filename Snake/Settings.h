9baab473d0aaed784cf66328a84d17a30b1b6918
hhayes0311 <23120037@student.hcmus.edu.vn>
2025-05-21 14:05:41 +0700

    Update Settings.h

--- Snake/Settings.h
+++ Snake/Settings.h
@@ -1,7 +1,7 @@
 ﻿#ifndef SETTINGS_H
 #define SETTINGS_H
 
-#include <SFML/Graphics.hpp>  // Needed for sf::VideoMode, sf::Uint32
+#include <SFML/Graphics.hpp>  
 #include <fstream> 
 
 #include "Config.h"
@@ -14,9 +14,9 @@ enum ResolutionOption {
 
 class GameSettings {
 public:
-    // default size
+    
     ResolutionOption resolution = RES_800x600;
-    // default volume
+   
     int volume = 100;  
 
     void save(std::ofstream& out);
@@ -24,12 +24,10 @@ public:
     void saveToFile();
     void loadFromFile();
 
-    // ✅ Declare these properly here
     sf::VideoMode getVideoMode() const;
     sf::Uint32 getWindowStyle() const;
 };
 
-// ✅ Global instance
 extern GameSettings setting;
 
-#endif // SETTINGS_H
+#endif 
